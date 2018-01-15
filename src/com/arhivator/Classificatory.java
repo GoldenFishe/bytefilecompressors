c51e04e9abfe50caa5f8c3311c0ef3bd68d1a2c9
Anton <funfun2010@mail.ru>
2018-01-16 01:42:38 +0300

    added fileSaver class

--- src/com/arhivator/Classificatory.java
+++ src/com/arhivator/Classificatory.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class Classificatory {
     byte[] bytes;
+    byte[] descriptionByte;
+
     ArrayList<Byte> bytesList = new ArrayList<>();
+    ArrayList<Integer> descriptionIndexList = new ArrayList<>();
+    ArrayList<Byte> descriptionByteList = new ArrayList<>();
 
     public Classificatory(byte[] data) {
         this.bytes = data;
-        for(byte bt: bytes) {
+        for (byte bt : bytes) {
             bytesList.add(bt);
         }
     }
 
-    public void setRange() {
+    public ArrayList<Byte> setRange() {
         int currentIndex = 0;
         int nextIndex;
 
@@ -25,7 +29,10 @@ public class Classificatory {
                     while (bytes[currentIndex] == bytes[nextIndex]) { // cycle for find same value
                         nextIndex++;
                     }
-                    replaceSubArray(currentIndex,nextIndex);
+                    descriptionIndexList.add(currentIndex);
+                    descriptionIndexList.add(nextIndex);
+                    descriptionByteList.add(bytes[currentIndex]);
+                    deleteSubArray(currentIndex, nextIndex);
                     currentIndex = nextIndex;                         // offset for correct next subRange
                 } else {
                     currentIndex++;
@@ -34,9 +41,23 @@ public class Classificatory {
                 currentIndex++;
             }
         }
+        return bytesList;
     }
-    private void replaceSubArray (int start, int end) {
-        bytesList.subList(start,end).clear();
-        System.out.println(start + " " + end);
+
+    public byte[] getDescriptionByte() {
+        int i = 0;
+        for (Byte bt : descriptionByteList) {
+            descriptionByte[i] = bt.byteValue();
+            i++;
+        }
+        return descriptionByte;
+    }
+
+    public ArrayList<Integer> getDescriptionIndexList() {
+        return descriptionIndexList;
+    }
+
+    private void deleteSubArray(int start, int end) {
+        bytesList.subList(start, end).clear();
     }
 }
--- src/com/arhivator/Main.java
+++ src/com/arhivator/Main.java
@@ -1,11 +1,19 @@
 package com.arhivator;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        String path = "C:/work/java/bytefilecompressors/src/test_data/test.jpg";
+        String pathGet = "C:/java/arhivator/src/test_data/test_txt.txt";
+        String pathSave = "C:/java/arhivator/src/test_data/test_saver.txt";
+
         FileReader fileReader = new FileReader();
-        byte[] data = fileReader.getFile(path);
+        byte[] data = fileReader.getFile(pathGet);
+
         Classificatory classificatory = new Classificatory(data);
-        classificatory.setRange();
+        ArrayList<Byte> byteArrayList = classificatory.setRange();
+
+        FileSaver fileSaver = new FileSaver(byteArrayList);
+        fileSaver.saveFile(pathSave);
     }
 }
